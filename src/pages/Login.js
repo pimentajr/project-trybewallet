04221e72516df6233e7bc2b9b33de2b778a58c2a
Thiago Gomes <thiagog.macedom@gmail.com>
2021-07-08 19:08:18 -0300

    requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,22 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <input
+          id="login"
+          data-testid="email-input"
+          placeholder="Campo de email"
+        />
+
+        <input
+          type="password"
+          data-testid="password-input"
+          placeholder="Campo de senha"
+        />
+
+        <input type="button" value="Entrar"/>
+      </div>);
   }
 }
 
