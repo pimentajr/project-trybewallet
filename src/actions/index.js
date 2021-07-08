49ca9a55ae91dbd32bfa6fdbe9dd0fe823425df7
heyynat <natalifplima@gmail.com>
2021-07-08 15:21:04 -0400

    Requisito 2

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,20 @@
 // Coloque aqui suas actions
+export const REQUEST_API = 'REQUEST_API';
+export const GET_PICTURE = 'GET_PICTURE';
+export const SING_IN = 'SING_IN';
+
+export const requestAPI = () => ({ type: REQUEST_API });
+
+export const getPicture = (data) => ({ type: GET_PICTURE, data });
+
+export function fetchAPI() {
+  // Desenvolva aqui o código da action assíncrona
+//   return (dispatch) => {
+//     return fetch("https://aws.random.cat/meow")
+//     .then((r) => r.json()
+//     .then(
+//       (json) => dispatch(getPicture(json)),
+//       (error) => dispatch(requestAPI(error)),
+//     ));
+//   };
+}
--- src/components/SingIn.jsx
+++ src/components/SingIn.jsx
@@ -1,18 +1,65 @@
 import React, { Component } from 'react';
+// import { useHistory } from 'react-router-dom';
 
 class SingIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.handleInput = this.handleInput.bind(this);
+    // this.YourComponent = this.YourComponent.bind(this);
+    this.disabled = this.disabled.bind(this);
+  }
+
+  handleInput(event) {
+    const { value, name } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  disabled() {
+    const { email, password } = this.state;
+    const minLength = 6;
+    // Expressao regular para validação de Email.
+    // LINK: https://stackoverflow.com/questions/4964691/super-simple-email-validation-with-javascript
+    const emailFormat = /^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,15}$/;
+    return (emailFormat.test(email) && password.length >= minLength);
+  }
+
+  // YourComponent() {
+  //   const history = useHistory();
+  //   history.push('/carteira');
+  // }
+
   render() {
+    const { email, password } = this.state;
     return (
       <div>
         <input
           data-testid="email-input"
           placeholder="Email"
+          type="email"
+          name="email"
+          value={ email }
+          onChange={ (e) => this.handleInput(e) }
         />
         <input
           data-testid="password-input"
           placeholder="Password"
+          type="text"
+          minLength="6"
+          name="password"
+          value={ password }
+          onChange={ (e) => this.handleInput(e) }
         />
-        <button type="button">Entrar</button>
+        <button
+          type="submit"
+          disabled={ !this.disabled() }
+          // onClick={ () => this.YourComponent() }
+        >
+          Entrar
+        </button>
       </div>
     );
   }
