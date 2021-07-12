de6fccb3b14f617c028cd9e8b20861e97073c74d
Michel Pereira <michelbecauro@gmail.com>
2021-07-11 21:11:57 -0300

    Criado estrutura redux

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <Switch>
+      (
+      <div>
+        Hello, TrybeWallet!
+      </div>
+      )
+      <Route exact path="/" component={ Login } />
+      <Route path="/carteira" render={ (props) => <Wallet { ...props } /> } />
+    </Switch>
+  );
 }
 
 export default App;
