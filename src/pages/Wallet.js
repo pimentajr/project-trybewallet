fc404192e1be92996a95663a8ad7c86f0a307e58
Michel Pereira <michelbecauro@gmail.com>
2021-07-12 13:53:04 -0300

    Feature Requisito 5 (Provisório)

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { getLogin } = this.props;
+    const TOTAL_INIT_VALUE = 0; // remover depois
+    const CAMBIO_INIT_VALUE = 'BRL'; // remover depois
+    return (
+      <div>
+        <div className="walletHeader">
+          <h3>TrybeWallet</h3>
+          <div className="walletHeaderRight">
+            <p className="email" data-testid="email-field">
+              { getLogin }
+            </p>
+            <p className="total" data-testid="total-field">
+              Total:
+              { TOTAL_INIT_VALUE }
+            </p>
+            <span data-testid="header-currency-field">
+              { CAMBIO_INIT_VALUE }
+            </span>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  getLogin: state.user.email,
+});
+
+Wallet.propTypes = {
+  getLogin: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Wallet);
