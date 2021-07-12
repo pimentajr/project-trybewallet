5df05b46aa149d7c392427d1f177c8305675a9f3
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2021-07-12 17:39:11 -0300

    Requisito 4 concluido

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import LoginPage from './components/LoginPage';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
   return (
     <Switch>
-      <Route exact path="/" component={ LoginPage } />
+      <Route exact path="/" component={ Login } />
+      <Route exact path="/carteira" component={ Wallet } />
     </Switch>
   );
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,102 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as EmailValidator from 'email-validator';
+import PropTypes from 'prop-types';
+import * as userAction from '../actions';
 
-class Login extends React.Component {
+class Login extends Component {
+  constructor() {
+    super();
+
+    this.handleChange = this.handleChange.bind(this);
+    this.addTaskOnStoreAndRedirect = this.addTaskOnStoreAndRedirect.bind(this);
+
+    this.state = {
+      email: '',
+      password: 0,
+      btnDisabled: true,
+    };
+  }
+
+  validateCredentials() {
+    const { email, password } = this.state;
+
+    const re = /^.{5,}$/;
+    if (EmailValidator.validate(email) && re.test(String(password))) {
+      this.setState({ btnDisabled: false });
+    } else {
+      this.setState({ btnDisabled: true });
+    }
+  }
+
+  handleChange({ target: { name, value } }) {
+    this.setState({
+      [name]: value,
+    });
+
+    this.validateCredentials();
+  }
+
+  addTaskOnStoreAndRedirect() {
+    const { email } = this.state;
+    const { addEmail } = this.props;
+
+    addEmail(email);
+
+    const { history } = this.props;
+
+    history.push('/carteira');
+  }
+
   render() {
-    return <div>Login</div>;
+    const { btnDisabled } = this.state;
+    return (
+      <div>
+        <form>
+          <label htmlFor="input-email">
+            Email:
+            <input
+              data-testid="email-input"
+              type="email"
+              placeholder="Digite seu email..."
+              name="email"
+              id="input-email"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label htmlFor="input-password">
+            Senha:
+            <input
+              data-testid="password-input"
+              type="password"
+              placeholder="Digite sua senha..."
+              name="password"
+              id="input-password"
+              onChange={ this.handleChange }
+            />
+          </label>
+        </form>
+        <button
+          disabled={ btnDisabled }
+          type="button"
+          onClick={ this.addTaskOnStoreAndRedirect }
+        >
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  addEmail: (email) => dispatch(userAction.addEmail(email)),
+});
+
+export default connect(null, mapDispatchToProps)(Login);
+
+Login.propTypes = {
+  addEmail: PropTypes.func.isRequired,
+  history: {
+    push: PropTypes.func,
+  }.isRequired,
+};
