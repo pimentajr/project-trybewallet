caf18077f21d3ea5b4f1eddeda46cc1e2f35283d
Michel Pereira <michelbecauro@gmail.com>
2021-07-11 22:25:25 -0300

    Feature 1 - Login.js - Estrutura

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,29 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div className="Login">
+        <h1>Login</h1>
+        <br />
+        <input
+          data-testid="email-input"
+          className="inputField"
+          type="text"
+          placeholder="E-mail"
+        />
+        <br />
+        <input className="inputField" type="password" placeholder="Senha" />
+        <br />
+        <button
+          data-testid="password-input"
+          lassName="inputField"
+          type="button"
+        >
+          Entrar
+        </button>
+
+      </div>
+    );
   }
 }
 
