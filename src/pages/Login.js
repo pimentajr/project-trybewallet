f80a0d6a1b1a66f37400e08a7c9423c7a688cb9b
Ana Carolina R Santos <kroline0693@gmail.com>
2021-07-13 19:35:40 -0300

    Requisito 1 completo

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,27 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <input
+          data-testid="email-input"
+          type="email"
+          placeholder="Email"
+          pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+          title="email inválido"
+          // onChange={ handleEmailChange }
+        />
+        <input
+          data-testid="password-input"
+          type="password"
+          placeholder="Senha"
+          pattern=".{6,}"
+          title="senha inválida"
+          // onChange={ handlePasswordChange }
+        />
+        <button type="button">Entrar</button>
+      </form>
+    );
   }
 }
 
