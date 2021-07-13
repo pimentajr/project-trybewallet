0eaefeb095caa5d2aca8410dae35c55d59e9b1d5
Halysson2 <halyssonsena83@gmail.comgit config --global user,name Halysson2padim123git config --global user.email halyssonsena83@gmail.com>
2021-07-13 15:09:10 -0300

    Requisito1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <div>
+          <form>
+            <input placeholder="e-mail" type="email" data-testid="email-input" />
+            <input placeholder="password" type="password" data-testid="password-input" />
+            <button type="button">Entrar</button>
+          </form>
+        </div>
+      </div>
+    );
   }
 }
 
