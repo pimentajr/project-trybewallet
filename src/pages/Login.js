5ae261efd1b97512d32b6a9da8d51fb83d897102
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-07-13 15:48:23 -0300

    Second requirement done

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,23 +1,62 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { email, password } = this.state;
+    const length = 6;
+
+    const emailRegex = () => {
+      const emailValid = /\S+@\S+\.\S+/;
+
+      const emailCorrect = emailValid.test(email);
+      return emailCorrect;
+    };
+
+    const passwordValid = password.length >= length;
+
     return (
       <div>
         <input
-          name="user"
+          name="email"
           type="email"
           data-testid="email-input"
           placeholder="Email"
+          value={ email }
+          onChange={ this.handleChange }
+
         />
         <input
           style={ { display: 'flex', marginTop: '10px' } }
-          name="password"
           type="password"
+          name="password"
           data-testid="password-input"
           placeholder="Senha"
+          value={ password }
+          onChange={ this.handleChange }
+
         />
-        <button type="submit">
+        <button
+          type="button"
+          disabled={ !(emailRegex() && passwordValid) }
+        >
           Entrar
         </button>
       </div>
