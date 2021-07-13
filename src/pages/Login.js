757915625049de16eeadf4ed786228e93ed4911f
nandovbr <nandovbr15@hotmail.com>
2021-07-13 17:34:13 -0300

    adiciona requisito 2 e início do 3

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,46 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { loginUserWallet } from '../actions';
+
+const characters = {
+  email: /^([a-z\d.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
+  password: /^[\w@-]{6,20}$/,
+};
 
 class Login extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      // email: '',
+      email: '',
+      password: '',
+      disabled: true,
     };
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+  validadeLogin() {
+    const { email, password } = this.state;
+    return characters.email.test(email) && characters.password.test(password);
+  }
+
+  handleInput(target) {
+    const { value, name } = target;
+    this.setState({
+      [name]: value,
+    }, () => this.setState({ disabled: !this.validadeLogin() }));
+  }
+
+  handleButton() {
+    const { submitButton, history } = this.props;
+    const { email } = this.state;
+    submitButton(email);
+    history.push('/carteira');
   }
 
   render() {
+    const { disabled } = this.state;
+
     return (
       <form>
         <label htmlFor="email">
@@ -17,6 +49,7 @@ class Login extends React.Component {
             data-testid="email-input"
             type="text"
             name="email"
+            onChange={ ({ target }) => this.handleInput(target) }
             placeholder="Digite seu email"
           />
         </label>
@@ -28,14 +61,30 @@ class Login extends React.Component {
             type="password"
             name="password"
             minLength="6"
+            onChange={ ({ target }) => this.handleInput(target) }
             placeholder="Digite sua senha"
           />
         </label>
 
-        <button type="button">Entrar</button>
+        <button
+          type="button"
+          onClick={ () => this.handleButton() }
+          disabled={ disabled }
+        >
+          Entrar
+        </button>
       </form>
     );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  submitButton: (value) => dispatch(loginUserWallet(value)),
+});
+
+Login.propTypes = {
+  submitButton: PropTypes.func.isRequired,
+  history: PropTypes.objectOf(PropTypes.object).isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
