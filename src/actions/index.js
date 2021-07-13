942500d4eb8dc3c71edf6d1ea43db5743993c932
Michelle <mls.srbek@gmail.com>
2021-07-13 20:43:27 -0300

    store and reducers configuration

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,9 @@
 // Coloque aqui suas actions
+export const LOGIN = 'LOGIN';
+
+export function login(payload) {
+  return {
+    type: LOGIN,
+    payload,
+  };
+}
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,17 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+import { LOGIN } from '../actions';
+
+const INITIAL_STATE = {
+  email: '',
+};
+
+function login(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case LOGIN:
+    return { ...state, email: action.payload };
+  default:
+    return state;
+  }
+}
+
+export default login;
