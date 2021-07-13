df76eb765db2f877ec5921a0994adaba6455b75b
pmaiamateus <pmaiamateus@gmail.com>
2021-07-13 17:26:44 -0300

    adiciona passo 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,28 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <section>
+        <h1>TrybeWallet Login</h1>
+        <form>
+          <input
+            type="email"
+            placeholder="Digite seu e-mail"
+            data-testid="email-input"
+          />
+          <input
+            type="password"
+            placeholder="Digite sua senha"
+            data-testid="password-input"
+          />
+          <button
+            type="submit"
+          >
+            Entrar
+          </button>
+        </form>
+      </section>
+    );
   }
 }
 
