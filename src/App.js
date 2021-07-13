319d23e7b5961813e0d9fb536956eb1db47fa855
Anajulia Brito <juliehindius@gmail.com>
2021-07-13 19:19:51 -0300

    Commit inicial

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Route, Router } from 'react-router-dom';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <Router>
+      <switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="Wallet" component={ Wallet } />
+      </switch>
+    </Router>);
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <input type="text" data-testid="email-input" />
+        <input type="text" data-testid="password-input" />
+        <button type="submit">Entrar</button>
+      </div>
+    );
   }
 }
 
