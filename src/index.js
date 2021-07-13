6c9bb35dfa8cadbfd8bf963c3c49572f5109da98
Halysson2 <halyssonsena83@gmail.comgit config --global user,name Halysson2padim123git config --global user.email halyssonsena83@gmail.com>
2021-07-13 15:26:53 -0300

    teste 1

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import store from './store';
 
 ReactDOM.render(
@@ -16,19 +14,7 @@ ReactDOM.render(
     ,
   </Provider>,
   document.getElementById('root'),
-=======
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
-import reducer from '../reducers';
 
-const store = createStore(
-  reducer,
-  composeWithDevTools(
-    applyMiddleware(thunk),
-  ),
->>>>>>> e036c2e0782c2099f679a62189765502f01f0bce
 );
 
 export default store;
-
