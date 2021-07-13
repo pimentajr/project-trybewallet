27a276a08c0bc1957739684e7911ae4c1a4d2c5b
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-07-12 21:57:19 -0300

    Done requirement 8.

---
+++ src/components/Table.js
@@ -0,0 +1,75 @@
+// import React from 'react';
+// // import PropTypes from 'prop-types';
+// // import { connect } from 'react-redux';
+// // import { fetchCurrency, addExpense } from '../actions';
+
+// class Table extends React.Component {
+//   // constructor(props) {
+//   //   super(props);
+//   //   this.state = {
+//   //     description: '',
+//   //     value: 0,
+//   //     currency: '',
+//   //     method: '',
+//   //     tag: '',
+//   //   };
+//   // }
+//   thead() {
+//     return (
+//       <tr>
+//         <th>Descrição</th>
+//         <th>Tag</th>
+//         <th>Método de pagamento</th>
+//         <th>Valor</th>
+//         <th>Moeda</th>
+//         <th>Câmbio utilizado</th>
+//         <th>Valor convertido</th>
+//         <th>Moeda de conversão</th>
+//         <th>Editar/Excluir</th>
+//       </tr>
+//     );
+//   }
+
+//   tbody() {
+//     return (
+//       <tr>
+//         <th>Descrição</th>
+//         <td>{}Tag</td>
+//         <td>{}Método de pagamento</td>
+//         <td>{}Valor</td>
+//         <td>{}Moeda</td>
+//         <td>{}Câmbio utilizado</td>
+//         <td>{}Valor convertido</td>
+//         <td>{}Moeda de conversão</td>
+//         <td>{}Editar/Excluir</td>
+//       </tr>
+//     );
+//   }
+
+//   render() {
+//     return (
+//       <table>
+//         {this.thead()}
+//         {this.tbody()}
+
+//       </table>
+//     )
+//   }
+
+//   // const mapStateToProps = (state) => ({
+//   //
+//   // });
+
+//   // const mapDispatchToProps = (dispatch) => ({
+//   //
+//   // });
+
+//   // ExpensesForm.propTypes = {
+//   //   APICurrency: PropTypes.func.isRequired,
+//   //   add: PropTypes.func.isRequired,
+//   //   walletExpenses: PropTypes.arrayOf(PropTypes.object).isRequired,
+//   //   currencies: PropTypes.objectOf(PropTypes.object).isRequired,
+//   // };
+// }
+
+// export default Table;
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -4,17 +4,29 @@ import { connect } from 'react-redux';
 import ExpensesForm from '../components/ExpensesForm';
 
 class Wallet extends React.Component {
-  render() {
-    const { email, walletExpenses } = this.props;
+  // String(walletExpenses.reduce((acc, cur) => acc + Number(cur.value), 0).toFixed(2),)
+  exchangeValue({ currency, value, exchangeRate }) {
+    const sumExchange = value * exchangeRate[currency];
+    return sumExchange;
+  }
 
+  InBLR(walletExpenses) {
+    const valueInBRL = walletExpenses.reduce((acc, expense) => {
+      const { value, currency, exchangeRates } = expense;
+      return acc + (parseFloat(value) * exchangeRates[currency].ask);
+    }, 0);
+    return valueInBRL;
+  }
+
+  render() {
+    const { email } = this.props;
+    const { walletExpenses } = this.props;
     return (
       <>
         <div>Logo</div>
         <div data-testid="email-field">{ email }</div>
         <p data-testid="total-field">
-          { `Despesa total: R$ ${String(
-            walletExpenses.reduce((acc, cur) => acc + Number(cur.value), 0).toFixed(2),
-          )} `}
+          {`Despesa total: R$ ${this.InBLR(walletExpenses).toFixed(2)}`}
         </p>
         <span data-testid="header-currency-field"> BRL</span>
         <ExpensesForm />
