7e8e6d372c32e5b23cc65f83e0be6f0b6c5e941c
Lucas Maestrelli <lucasmaestrelli95@gmail.com>
2021-07-13 15:17:59 -0300

    req5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,14 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Forms from '../components/Forms';
 
 class Wallet extends React.Component {
   render() {
+    const { email } = this.props;
     return (
       <div>
-        <header />
+        <header>
+          <p data-testid="email-field">{ email }</p>
+          <p data-testid="total-field">0</p>
+          <p data-testid="header-currency-field">BRL</p>
+        </header>
         <Forms />
       </div>);
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Wallet);
