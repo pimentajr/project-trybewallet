71939c4aa2a5e478ef223e74355437f9ed84c21a
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-07-13 17:13:04 -0300

    five requirement done

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,16 @@
 // Coloque aqui suas actions
 export const LOGIN_USER = 'LOGIN_USER';
+export const SHOW_EMAIL = 'SHOW_EMAIL';
 
 export const userLogin = (email) => ({
   type: LOGIN_USER,
+  user: {
+    email,
+  },
+});
+
+export const viewEmail = (email) => ({
+  type: SHOW_EMAIL,
   user: {
     email,
   },
