68638c3062acfa84aa747c46bf5f3114c268383a
Michelle <mls.srbek@gmail.com>
2021-07-13 19:28:43 -0300

    email input

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div className="login-container">
+        <h3>Login</h3>
+        <label htmlFor="email-login">
+          Email:
+          <input data-testid="email-input" type="text" name="email" />
+        </label>
+      </div>
+    );
   }
 }
 
