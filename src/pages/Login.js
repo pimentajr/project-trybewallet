9b5acbf19a1742d58a88a88e4d91c7c8b9ad07cd
Michelle <mls.srbek@gmail.com>
2021-07-13 20:51:00 -0300

    disable button

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+      disable: true,
+      redirect: false,
+    };
+  }
+
   render() {
+    const { disable } = this.state;
     return (
       <div className="login-container">
         <h3>Login</h3>
@@ -16,7 +28,7 @@ class Login extends React.Component {
             placeholder="Senha"
           />
         </label>
-        <button type="button">Entrar</button>
+        <button type="button" disabled={ disable }>Entrar</button>
       </div>
     );
   }
