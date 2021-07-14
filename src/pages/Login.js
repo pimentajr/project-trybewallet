57cb10a3874ea8d3abc3df9c9bfec9f74c298c2d
benlopes <rubenssouss@hotmail.com>
2021-07-14 10:40:28 -0300

    Add email and password validations

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,18 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class Login extends React.Component {
-  render() {
-    return (
-      <div>
-        <label htmlFor="email-input">
-          <input type="email" data-testid="email-input" />
-        </label>
-        <label htmlFor="password-input">
-          <input type="password" data-testid="password-input" />
-        </label>
-        <button type="button">Entrar</button>
-      </div>);
+function Login() {
+  const [email, setEmail] = useState();
+  const [pass, setPass] = useState();
+
+  function checkEmail(e, p) {
+    const validEmail = /^[a-zA-Z]+@[a-zA-Z]+\.[com]{3,}$/i;
+    const passLength = 6;
+    return validEmail.test(e) && p.length >= passLength;
   }
+
+  return (
+    <div>
+      <label htmlFor="email-input">
+        <input
+          value={ email }
+          type="email"
+          data-testid="email-input"
+          required
+          onChange={ (e) => setEmail(e.target.value) }
+        />
+      </label>
+      <label htmlFor="password-input">
+        <input
+          value={ pass }
+          type="password"
+          data-testid="password-input"
+          required
+          onChange={ (e) => setPass(e.target.value) }
+        />
+      </label>
+      <button
+        type="button"
+        disabled={ email && pass ? !checkEmail(email, pass) : true }
+      >
+        Entrar
+      </button>
+    </div>
+  );
 }
 
 export default Login;
