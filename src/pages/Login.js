68c21bb6fe2d64479f67a7f66df2788b668b7be1
Pedrodev1 <pedro.oliveira.dev@outlook.com>
2021-07-13 21:50:06 -0300

    Pagina Login finalizada

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,93 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { setEmail } from '../actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      btnDisable: false,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.submitInputEmail2State = this.submitInputEmail2State.bind(this);
+  }
+
+  authenticate() {
+    const { email, password } = this.state;
+    const minLength = 5;
+    this.setState({
+      btnDisable: password.length >= minLength && email.includes('.com'),
+    });
+  }
+
+  handleChange({ target: { name, value } }) {
+    this.setState({
+      [name]: value,
+    }, this.authenticate());
+  }
+
+  submitInputEmail2State() {
+    const { submitEmail, history } = this.props;
+    const { email } = this.state;
+
+    submitEmail(email);
+    history.push('/carteira');
+  }
+
   render() {
-    return <div>Login</div>;
+    const { btnDisable } = this.state;
+    return (
+      <div className="Login">
+        <form className="formLogin">
+          <label htmlFor="email" className="login-label">
+            <input
+              type="text"
+              name="email"
+              data-testid="email-input"
+              id="email"
+              placeholder="Email"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label htmlFor="password" className="password-label">
+            <input
+              type="password"
+              name="password"
+              data-testid="password-input"
+              placeholder="Senha"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <button
+            type="button"
+            disabled={ !btnDisable }
+            onClick={ this.submitInputEmail2State }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  submitEmail: (payload) => dispatch(setEmail(payload)),
+});
+
+Login.propTypes = {
+  submitEmail: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    location: PropTypes.shape({
+      pathname: PropTypes.string.isRequired,
+      search: PropTypes.string.isRequired,
+      hash: PropTypes.string.isRequired,
+    }),
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,21 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+import { SET_EMAIL } from '../actions/index';
+
+const INITIAL_STATE = {
+  email: '',
+  password: '',
+  redirect: false,
+};
+
+function user(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case SET_EMAIL:
+    return { ...state,
+      email: action.payload,
+    };
+  default:
+    return state;
+  }
+}
+
+export default user;
