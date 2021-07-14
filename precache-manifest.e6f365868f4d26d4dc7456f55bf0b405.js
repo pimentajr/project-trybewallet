d8fce0b7202112c52806a5ded0ff42b2072e004e
Matheus Gaspar <msouzagaspar@gmail.com>
2021-07-14 11:39:04 -0300

    Updates

---
+++ precache-manifest.e6f365868f4d26d4dc7456f55bf0b405.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23e5d47c9294e2adc44ade0cb827accd",
+    "url": "/trybe-trybewallet/index.html"
+  },
+  {
+    "revision": "cc8bf47b662bf0eaba33",
+    "url": "/trybe-trybewallet/static/css/main.babd03ca.chunk.css"
+  },
+  {
+    "revision": "d785047a950c6b0a68e5",
+    "url": "/trybe-trybewallet/static/js/2.b8872b92.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybe-trybewallet/static/js/2.b8872b92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc8bf47b662bf0eaba33",
+    "url": "/trybe-trybewallet/static/js/main.f366ee37.chunk.js"
+  },
+  {
+    "revision": "97acac5d28a5fa3274d2",
+    "url": "/trybe-trybewallet/static/js/runtime-main.a6aee2ef.js"
+  }
+]);
