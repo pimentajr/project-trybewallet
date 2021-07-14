21d2f1c28ca802ed901823177db66ee164e55684
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-07-14 01:47:06 -0300

    Refatora valores iniciais do form

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,26 +1,47 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Form extends Component {
+  constructor() {
+    super();
+    this.state = {
+      value: 0,
+      description: '',
+      currency: 'USD',
+      payment: 'Dinheiro',
+      tag: 'Alimentação',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(name, value) {
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { value, description, currency, payment, tag } = this.state;
+    const { currencies } = this.props;
     return (
       <form>
         <label htmlFor="value">
           Valor
-          <input type="text" id="value" />
+          <input type="text" id="value" value={ value } />
         </label>
         <label htmlFor="description">
           Descrição
-          <input type="text" id="description" />
+          <input type="text" id="description" value={ description } />
         </label>
         <label htmlFor="currency">
           Moeda
-          <select id="currency">
-            <option>teste</option>
+          <select id="currency" value={ currency } onChange={ this.handleChange }>
+            { currencies.map((item, index) => <option key={ index }>{ item }</option>) }
           </select>
         </label>
         <label htmlFor="payment">
           Método de pagamento
-          <select id="payment">
+          <select id="payment" value={ payment } onChange={ this.handleChange }>
             <option value="Dinheiro">Dinheiro</option>
             <option value="Cartão de crédito">Cartão de crédito</option>
             <option value="Cartão de débito">Cartão de débito</option>
@@ -28,7 +49,7 @@ class Form extends Component {
         </label>
         <label htmlFor="expense">
           Tag
-          <select id="expense">
+          <select id="expense" value={ tag } onChange={ this.handleChange }>
             <option value="alimentacao">Alimentação</option>
             <option value="lazer">Lazer</option>
             <option value="trabalho">Trabalho</option>
@@ -40,5 +61,9 @@ class Form extends Component {
     );
   }
 }
+
+Form.propTypes = {
+  currencies: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
 
 export default Form;
