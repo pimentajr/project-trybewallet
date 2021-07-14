10dc74ec9d3abce3d273e7dc94c718450e6ca7f5
IgorMendez <igor.emp@hotmail.com>
2021-07-14 05:48:52 -0300

    Exercício 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <input data-testid="email-input" />
+        <input data-testid="password-input" />
+        <button type="submit">Entrar</button>
+      </form>
+    );
   }
 }
 
