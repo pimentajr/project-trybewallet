a9a27758dbe654ec3bfd1950d13577f58e038266
Michelle <mls.srbek@gmail.com>
2021-07-14 15:57:44 -0300

    import correction

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import login from '../actions';
+import { login } from '../actions';
 
 class Login extends React.Component {
   constructor() {
