759e16e83860556c8566c334d44313fef97cf14d
IgorMendez <igor.emp@hotmail.com>
2021-07-14 06:24:54 -0300

    Exercício 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,68 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      disabled: true,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleCheck = this.handleCheck.bind(this);
+  }
+
+  componentDidUpdate() {
+    this.handleCheck();
+  }
+
+  handleCheck() {
+    const { email, password, disabled } = this.state;
+    const num = 6;
+    const emailCheck = email.split('').includes('@') && email.split('.').includes('com');
+    const passwordCheck = password.length >= num;
+    if (emailCheck && passwordCheck && disabled) {
+      this.setState({
+        disabled: false,
+      });
+    } else if ((!emailCheck || !passwordCheck) && !disabled) {
+      this.setState({
+        disabled: true,
+      });
+    }
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { email, password, disabled } = this.state;
     return (
       <form>
-        <input data-testid="email-input" />
-        <input data-testid="password-input" />
-        <button type="submit">Entrar</button>
+        <label htmlFor="email">
+          <input
+            name="email"
+            type="email"
+            value={ email }
+            onChange={ this.handleChange }
+            data-testid="email-input"
+          />
+        </label>
+        <label htmlFor="password">
+          <input
+            name="password"
+            minLength="6"
+            value={ password }
+            onChange={ this.handleChange }
+            data-testid="password-input"
+          />
+        </label>
+        <button disabled={ disabled } type="submit">Entrar</button>
       </form>
     );
   }
