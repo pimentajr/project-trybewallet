ab4d5e37259891fb7cffd71ea65d6dab6c119ab6
CLIENTE <prih.scapin@gmail.com>
2021-07-14 16:10:20 -0300

    Requisito 2

--- src/index.js
+++ src/index.js
@@ -8,11 +8,10 @@ import * as serviceWorker from './serviceWorker';
 import store from './store';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
   <Provider store={ store }>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root'),
 );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,74 @@
 import React from 'react';
-import LoginButton from '../components/LoginButton';
+import { Link } from 'react-router-dom';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      disabled: true,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+
+  componentDidUpdate() {
+    this.handleLogin();
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleLogin() {
+    const { email, password, disabled } = this.state;
+    const validação = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
+    const validSenha = 6;
+    if (validação.test(email) && password.length >= validSenha && disabled) {
+      this.setState({ disabled: false });
+    } else if ((!validação.test(email) || !password.length >= validSenha) && !disabled) {
+      this.setState({ disabled: true });
+    }
+  }
+
   render() {
+    const { email, password, disabled } = this.state;
     return (
       <div>
         Login
-        <input data-testid="email-input" placeholder="E-mail" />
-        <input data-testid="password-input" placeholder="Senha" />
-        <LoginButton />
+        <input
+          name="email"
+          data-testid="email-input"
+          placeholder="E-mail"
+          value={ email }
+          type="email"
+          onChange={ this.handleChange }
+        />
+
+        <input
+          name="password"
+          data-testid="password-input"
+          placeholder="Senha"
+          value={ password }
+          type="password"
+          minLength="6"
+          onChange={ this.handleChange }
+        />
+        <div>
+          <Link to="/carteira">
+            <button
+              type="button"
+              disabled={ disabled }
+            // onClick={}
+            >
+              Entrar
+            </button>
+          </Link>
+        </div>
       </div>
     );
   }
