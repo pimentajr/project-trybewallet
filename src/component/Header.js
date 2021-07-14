aaa6a888d3b9a708cbb34cd09f46ed259f694659
Arlesson Moura <arlessonmss@gmail.com>
2021-07-14 13:52:44 -0300

    Feat: Is It the last assault? II

--- src/component/Header.js
+++ src/component/Header.js
@@ -3,12 +3,27 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.totalExpense = this.totalExpense.bind(this);
+  }
+
+  totalExpense() {
+    const { expenses } = this.props;
+    const total = expenses.reducer((acc, element) => {
+      const { value, currency, askCurrency } = element;
+      const { ask } = Object.values(askCurrency).find(({ code }) => code === currency);
+      return acc + (ask * value);
+    }, 0);
+    return total;
+  }
+
   render() {
     const { email } = this.props;
     return (
       <header>
         <p data-testid="email-field">{ email }</p>
-        <p data-testid="total-field">0</p>
+        <p data-testid="total-field">{ this.totalExpense() }</p>
         <p data-testid="header-currency-field">BRL</p>
       </header>
     );
@@ -18,13 +33,13 @@ class Header extends Component {
 const mapStateToProps = (state) => ({
   email: state.user.email,
   expenses: state.wallet.expenses,
-  currencies: state.wallet.currencies,
+  // currencies: state.wallet.currencies,
 });
 
 Header.propTypes = {
   email: PropTypes.string,
   expenses: PropTypes.number,
-  currencies: PropTypes.number,
+  // currencies: PropTypes.number,
 }.isRequired;
 
 export default connect(mapStateToProps)(Header);
