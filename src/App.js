073fa7f548ee82cdf5cd85c6c35f9c8288c65454
benlopes <rubenssouss@hotmail.com>
2021-07-14 08:17:34 -0300

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <div>
+      <label htmlFor="email-input">
+        <input type="email" data-testid="email-input" />
+      </label>
+      <label htmlFor="password-input">
+        <input type="password" data-testid="password-input" />
+      </label>
+      <button type="button">Entrar</button>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './index.css';
+// import { Provider } from 'react-redux';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
+  // <Provider>
   <BrowserRouter>
-    <App />
+    <Route path="/" component={ App } />
+    {/* <App /> */}
   </BrowserRouter>,
+  // </Provider>,
   document.getElementById('root'),
 );
 
