a468ce6a4c41af7ec1e13eeab1974bda593a8f00
benlopes <rubenssouss@hotmail.com>
2021-07-15 09:13:45 -0300

    Add 5th requirement

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import Wrapper from './Wallet.styles';
 
 function Wallet() {
+  const userEmail = useSelector((state) => state.user.email);
   return (
     <div>
-      <header className="walletHeader">
-        <div data-testid="email-field">email</div>
-        <div data-testid="total-field">total expense</div>
+      <Wrapper>
+        TrybeWallet
+        <div data-testid="email-field">
+          Email:
+          {userEmail}
+        </div>
+        <div data-testid="total-field">
+          <span>Despesa total: R$ </span>
+          0
+        </div>
         <div data-testid="header-currency-field">BRL</div>
-      </header>
-      TrybeWallet
+      </Wrapper>
     </div>
   );
 }
---
+++ src/pages/Wallet.styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.header`
+  display: flex;
+  justify-content: space-around;
+`;
+
+export default Wrapper;
