599c9403fd0e89f991e97060bbefecad8f60ea2f
Luiz Putinatti <luiz_edu16@hotmail.com>
2021-07-14 21:33:55 -0300

    iniciando projeto

---
+++ src/components/Form.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { coinsThunk } from '../actions/index';
+
+class Form extends React.Component {
+  componentDidMount() {
+    const { coins } = this.props;
+    coins();
+  }
+
+  render() {
+    const { currencies } = this.props;
+    return (
+      <div>
+        <form>
+          <label htmlFor="valor">
+            Valor
+            <input id="valor" type="number" name="valor" />
+          </label>
+          <label htmlFor="describle">
+            Descrição
+            <input id="describle" type="text" name="describle" />
+          </label>
+          <label htmlFor="moeda">
+            Moeda
+            <select id="moeda" name="moeda">
+              {!currencies
+                ? <option value="BRL">BRL</option>
+                : currencies.map((currency) => (
+                  <option key={ currency } value={ currency }>{ currency }</option>
+                ))}
+            </select>
+          </label>
+          <label htmlFor="metodo">
+            Método de pagamento
+            <select id="metodo" name="metodo">
+              <option>Dinheiro</option>
+              <option>Cartão de crédito</option>
+              <option>Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="tag">
+            Tag
+            <select id="tag" name="tag">
+              <option>Alimentação</option>
+              <option>Lazer</option>
+              <option>Trabalho</option>
+              <option>Transporte</option>
+              <option>Saúde</option>
+            </select>
+          </label>
+          <button type="submit">
+            Adicionar Despesas
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  currencies: state.wallet.currencies,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  coins: () => dispatch(coinsThunk()),
+});
+
+Form.propTypes = {
+  getCoins: PropTypes.func,
+}.isRequired;
+
+export default connect(mapStateToProps, mapDispatchToProps)(Form);
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,96 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { saveEmail } from '../actions';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    // this.handleClick = this.handleClick.bind(this);
+
+    this.state = {
+      email: '',
+      password: '',
+      buttonOk: false,
+    };
+  }
+
+  // regex utilizado do projeto em grupo com amigos.
+  validateFields() {
+    const CARACTER_MIN = 6;
+    const { password, email } = this.state;
+    const regex = /^[\w.]+@[a-z]+\.\w{2,3}$/g;
+    const buttonOk = password.length >= CARACTER_MIN && regex.test(email);
+    this.setState({ buttonOk });
+  }
+
+  handleChange({ target }) {
+    const { id, value } = target;
+    this.setState({
+      [id]: value,
+    }, () => this.validateFields());
+  }
+
+  // handleClick() {
+  //   const { email } = this.state;
+  //   const { save } = this.props;
+  //   save(email);
+  // }
+
   render() {
-    return <div>Login</div>;
+    const { email, password, buttonOk } = this.state;
+    const { save } = this.props;
+
+    return (
+      <div>
+        <form>
+          <label htmlFor="email">
+            E-mail
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={ email }
+              placeholder="Digite seu e-mail"
+              data-testid="email-input"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label htmlFor="password">
+            Senha
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={ password }
+              placeholder="Digite sua senha"
+              data-testid="password-input"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <Link to="/carteira">
+            <button
+              type="button"
+              disabled={ !buttonOk }
+              onClick={ () => save(email) }
+            >
+              Entrar
+            </button>
+          </Link>
+        </form>
+      </div>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  save: (email) => dispatch(saveEmail(email)),
+});
+
+Login.propTypes = {
+  save: PropTypes.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
