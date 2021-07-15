19d6397cca5c1126e19dc5725044de25ec126d9e
Anajulia Brito <juliehindius@gmail.com>
2021-07-15 11:20:09 -0300

    projetinho, agora vai

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Route, Router } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
 
 function App() {
   return (
     <Router>
-      <switch>
+      <Switch>
         <Route exact path="/" component={ Login } />
         <Route path="Wallet" component={ Wallet } />
-      </switch>
+      </Switch>
     </Router>);
 }
 
