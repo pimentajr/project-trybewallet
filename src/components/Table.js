a15e52df776429ad336cc62d265d8728bd504eea
getuliogutemberg <getulio.dev@gmail.com>
2021-07-15 06:11:24 -0300

    resolve lint

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';//
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import * as actions from '../actions';
 
 class Table extends Component {
