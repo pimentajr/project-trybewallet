04585c6f8155f441945835b143f64242f1d269de
tomsantos07 <xotmsantosx@live.com>
2021-07-15 19:58:05 -0300

    completando a action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,27 @@
 // Coloque aqui suas actions
+export const LOGIN_USER = 'LOGIN_USER';
+export const NEW_EXPENSE = 'NEW_EXPENSE';
+
+export const userLogin = (email) => ({
+  type: LOGIN_USER,
+  user: {
+    email,
+  },
+});
+export const addNewExpense = (expense) => ({
+  type: NEW_EXPENSE,
+  expense,
+});
+
+export const fetchAPIExpenseAction = (expense) => async (dispatch) => {
+  const URL = 'https://economia.awesomeapi.com.br/json/all';
+
+  const fetchAPI = await fetch(URL);
+  const parseJSON = await fetchAPI.json();
+
+  dispatch(addNewExpense({
+    ...expense,
+    exchangeRate: parseJSON,
+    exchangeRates: parseJSON,
+  }));
+};
