253f8eb0dbd138629b97797a65ea708a04e39244
tomsantos07 <xotmsantosx@live.com>
2021-07-15 13:45:45 -0300

    browser router e provider

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,24 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+
+import { NEW_EXPENSE } from '../actions';
+
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+};
+
+const userReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case NEW_EXPENSE:
+    return {
+      ...state,
+      expenses: [
+        ...state.expenses,
+        action.expense,
+      ],
+    };
+  default:
+    return state;
+  }
+};
+export default userReducer;
