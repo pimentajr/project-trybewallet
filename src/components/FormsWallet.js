66535a4a99f936e1ea0c9613d83676e97a41bbb2
Halysson2 <halyssonsena83@gmail.comgit config --global user,name Halysson2padim123git config --global user.email halyssonsena83@gmail.com>
2021-07-16 13:09:48 -0300

    requisito 6

--- src/components/FormsWallet.js
+++ src/components/FormsWallet.js
@@ -1,44 +1,52 @@
-import React from 'react';
+// import React, { Component } from 'react';
 
-class FormsWallet extends React.Component {
-  render() {
-    return (
-      <form>
-        <label htmlFor="valor">
-          Valor:
-          <input name="valor" type="number" id="valor" />
-        </label>
-        <label htmlFor="descricao">
-          Descrição:
-          <input name="descricao" type="text" id="descricao" />
-        </label>
-        <label htmlFor="moeda">
-          Moeda:
-          <select>
-            <option value="dinheiro">Dinheiro</option>
-          </select>
-        </label>
-        <label htmlFor="metodo-de-pagamento">
-          Método de pagamento:
-          <select id="metodo-de-pagamento">
-            <option value="dinheiro">Dinheiro</option>
-            <option value="cartao-de-credito">Cartão de crédito</option>
-            <option value="cartao-de-debito">Cartão de débito</option>
-          </select>
-        </label>
-        <label htmlFor="categoria">
-          Tag:
-          <select id="categoria">
-            <option value="alimentacao">Alimentação</option>
-            <option value="lazer">Lazer</option>
-            <option value="trabalho">Trabalho</option>
-            <option value="transporte">Transporte</option>
-            <option value="saude">Saúde</option>
-          </select>
-        </label>
-      </form>
-    );
-  }
-}
+// class FormsWallet extends Component {
+//   render() {
+//     return (
+//       <form>
+//         <label htmlFor="valor">
+//           Valor:
+//           <input
+//             name="valor"
+//             type="number"
+//             id="valor"
+//           />
+//         </label>
+//         <label htmlFor="descricao">
+//           Descrição:
+//           <input
+//             name="descricao"
+//             type="text"
+//             id="descricao"
+//           />
+//         </label>
+//         <label htmlFor="moeda">
+//           Moeda:
+//           <select id="moeda">
+//             <option value="test">Teste</option>
+//           </select>
+//         </label>
+//         <label htmlFor="metodo-de-pagamento">
+//           Método de pagamento:
+//           <select>
+//             <option value="dinheiro">Dinheiro</option>
+//             <option value="cartao-de-credito">Cartão de crédito</option>
+//             <option value="cartao-de-debito">Cartão de débito</option>
+//           </select>
+//         </label>
+//         <label htmlFor="categoria">
+//           Tag:
+//           <select>
+//             <option value="alimentacao">Alimentação</option>
+//             <option value="lazer">Lazer</option>
+//             <option value="trabalho">Trabalho</option>
+//             <option value="transporte">Transporte</option>
+//             <option value="saude">Saúde</option>
+//           </select>
+//         </label>
+//       </form>
+//     );
+//   }
+// }
 
-export default FormsWallet;
+// export default FormsWallet;
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,31 +1,59 @@
+import { connect } from 'react-redux';
 import React from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import FormsWallet from '../components/FormsWallet';
 
 class Wallet extends React.Component {
   render() {
-    const { userEmail } = this.props;
+    const { email } = this.props;
     return (
       <div>
-        TrybeWallet
         <header>
-          <p data-testid="email-field">{`Usuário: ${userEmail} `}</p>
-          <p data-testid="total-field">0</p>
-          <p data-testid="header-currency-field">BRL</p>
+          <p data-testid="email-field">{email}</p>
+          <p data-testid="total-field"> 0 </p>
+          <p data-testid="header-currency-field"> BRL </p>
         </header>
-        <FormsWallet />
+        <form>
+          <label htmlFor="input-value">
+            Valor
+            <input id="input-value" type="text" />
+          </label>
+          <label htmlFor="descrição">
+            Descrição
+            <input id="descrição" type="text" />
+          </label>
+          <label htmlFor="currency">
+            Moeda
+            <select role="combobox" id="currency">
+              <option value="dinheiro">Dinheiro</option>
+            </select>
+          </label>
+          <label htmlFor="metodo-de-pagamento">
+            Método de Pagamento
+            <select role="combobox" id="payment-method">
+              <option value="dinheiro"> Dinheiro</option>
+              <option value="cartao-de-credito">Cartão de crédito</option>
+              <option value="cartao-de-debito">Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="tag">
+            Tag
+            <select role="combobox" id="tag">
+              <option value="alimentação">Alimentação</option>
+              <option value="lazer">Lazer</option>
+              <option value="trabalho">Trabalho</option>
+              <option value="transporte">Transporte</option>
+              <option value="saude">Saúde</option>
+            </select>
+          </label>
+        </form>
       </div>
     );
   }
 }
-
 const mapStateToProps = (state) => ({
-  userEmail: state.user.email,
+  email: state.user.email,
 });
-
-export default connect(mapStateToProps)(Wallet);
-
 Wallet.propTypes = {
-  userEmail: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
 };
+export default connect(mapStateToProps)(Wallet);
