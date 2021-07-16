c1a8112de7009cefc844594650c5259d7f322a9f
Luiz Putinatti <luiz_edu16@hotmail.com>
2021-07-15 22:51:24 -0300

    requisito 8- inclusao das informaçoes no state local

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,40 +1,83 @@
+/* eslint-disable max-lines-per-function */
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { coinsThunk } from '../actions/index';
 
 class Form extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      value: 0,
+      description: '',
+      currency: 'USD',
+      method: 'Dinheiro',
+      tag: 'Alimentação',
+    };
+
+    this.handleChance = this.handleChance.bind(this);
+  }
+
   componentDidMount() {
     const { coins } = this.props;
     coins();
   }
 
+  handleChance({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+    console.log(this.state);
+  }
+
   render() {
+    const { value, currency, method, tag, description } = this.state;
     const { currencies } = this.props;
     return (
       <div>
         <form>
           <label htmlFor="valor">
             Valor
-            <input id="valor" type="number" name="valor" />
+            <input
+              id="valor"
+              value={ value }
+              type="number"
+              name="value"
+              onChange={ this.handleChance }
+            />
           </label>
           <label htmlFor="describle">
             Descrição
-            <input id="describle" type="text" name="describle" />
+            <input
+              id="describle"
+              type="text"
+              name="description"
+              value={ description }
+              onChange={ this.handleChance }
+            />
           </label>
           <label htmlFor="moeda">
             Moeda
-            <select id="moeda" name="moeda">
+            <select
+              id="moeda"
+              name="currency"
+              value={ currency }
+              onChange={ this.handleChance }
+            >
               {!currencies
                 ? <option value="BRL">BRL</option>
-                : currencies.map((currency) => (
-                  <option key={ currency } value={ currency }>{ currency }</option>
+                : currencies.map((currenc) => (
+                  <option key={ currenc } value={ currenc }>{ currenc }</option>
                 ))}
             </select>
           </label>
           <label htmlFor="metodo">
             Método de pagamento
-            <select id="metodo" name="metodo">
+            <select
+              id="metodo"
+              name="method"
+              value={ method }
+              onChange={ this.handleChance }
+            >
               <option>Dinheiro</option>
               <option>Cartão de crédito</option>
               <option>Cartão de débito</option>
@@ -42,7 +85,12 @@ class Form extends React.Component {
           </label>
           <label htmlFor="tag">
             Tag
-            <select id="tag" name="tag">
+            <select
+              id="tag"
+              name="tag"
+              value={ tag }
+              onChange={ this.handleChance }
+            >
               <option>Alimentação</option>
               <option>Lazer</option>
               <option>Trabalho</option>
