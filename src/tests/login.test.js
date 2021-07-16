045f955f20602e0bb78795296b49b893710f0145
LukeCruz <enio.cruz@yahoo.com.br>
2021-07-16 15:22:43 -0300

    a

--- src/tests/login.test.js
+++ src/tests/login.test.js
@@ -3,25 +3,20 @@ import userEvent from '@testing-library/user-event';
 import { fireEvent, screen } from '@testing-library/react';
 import App from '../App';
 
-import { renderWithRouterAndStore } from './testConfig';
-
-const EMAIL_INPUT_TEST_ID = 'email-input';
-const PASSWORD_INPUT_TEST_ID = 'password-input';
-const VALID_EMAIL = 'alguem@email.com';
-const VALID_PASSWORD = '123456';
+import { renderWithRouterAndStore } from './testConfig'
 
 afterEach(() => jest.clearAllMocks());
 
 describe('1 - Crie uma página inicial de login com os seguintes campos e características:', () => {
   test('A rota para esta página deve ser \'/\'', () => {
-    const { history } = renderWithRouterAndStore(<App />, '/');
+    const { history } = renderWithRouterAndStore(<App />);
     expect(history.location.pathname).toBe('/');
   });
 
   test('Crie um local para que o usuário insira seu email e senha', () => {
     renderWithRouterAndStore(<App />, '/');
-    const email = screen.getByTestId(EMAIL_INPUT_TEST_ID);
-    const senha = screen.getByTestId(PASSWORD_INPUT_TEST_ID);
+    const email = screen.getByTestId('email-input');
+    const senha = screen.getByTestId('password-input');
 
     expect(email).toBeInTheDocument();
     expect(senha).toBeInTheDocument();
@@ -33,87 +28,62 @@ describe('1 - Crie uma página inicial de login com os seguintes campos e caract
     const button = screen.getByText(/Entrar/i);
     expect(button).toBeInTheDocument();
   });
-});
 
-describe('2 - Realize as seguintes verificações nos campos de email, senha e botão:', () => {
-  test('O botão de "Entrar" está desabilitado ao entrar na página', () => {
-    renderWithRouterAndStore(<App />, '/');
+  test('Realize as seguintes verificações nos campos de email, senha e botão:', () => {
+    renderWithRouterAndStore(<App />);
 
     const button = screen.getByText(/Entrar/i);
     expect(button).toBeDisabled();
-  });
 
-  test('O botão de "Entrar está desabilitado quando um email inválido é digitado', () => {
-    renderWithRouterAndStore(<App />, '/');
-
-    const email = screen.getByTestId(EMAIL_INPUT_TEST_ID);
-    const senha = screen.getByTestId(PASSWORD_INPUT_TEST_ID);
-    const button = screen.getByText(/Entrar/i);
+    const email = screen.getByTestId('email-input');
+    const senha = screen.getByTestId('password-input');
 
     userEvent.type(email, 'email');
-    userEvent.type(senha, VALID_PASSWORD);
+    userEvent.type(senha, '123456');
     expect(button).toBeDisabled();
 
     userEvent.type(email, 'email@com@');
-    userEvent.type(senha, VALID_PASSWORD);
+    userEvent.type(senha, '123456');
     expect(button).toBeDisabled();
 
     userEvent.type(email, 'emailcom@');
-    userEvent.type(senha, VALID_PASSWORD);
+    userEvent.type(senha, '123456');
+    expect(button).toBeDisabled();
+
+    userEvent.type(email, 'alguem@email.com');
+    userEvent.type(senha, '23456');
     expect(button).toBeDisabled();
 
     userEvent.type(email, 'alguem@email.');
-    userEvent.type(senha, VALID_PASSWORD);
+    userEvent.type(senha, '123456');
     expect(button).toBeDisabled();
-  });
 
-  test('O botão de "Entrar está desabilitado quando uma senha inválida é digitada', () => {
-    renderWithRouterAndStore(<App />, '/');
-
-    const email = screen.getByTestId(EMAIL_INPUT_TEST_ID);
-    const senha = screen.getByTestId(PASSWORD_INPUT_TEST_ID);
-    const button = screen.getByText(/Entrar/i);
-
-    userEvent.type(email, VALID_EMAIL);
-    userEvent.type(senha, '23456');
-    expect(button).toBeDisabled();
-  });
-
-  test('O botão de "Entrar" está habilitado quando um email e uma senha válidos são passados', () => {
-    renderWithRouterAndStore(<App />, '/');
-
-    const email = screen.getByTestId(EMAIL_INPUT_TEST_ID);
-    const senha = screen.getByTestId(PASSWORD_INPUT_TEST_ID);
-    const button = screen.getByText(/Entrar/i);
-
-    userEvent.type(email, VALID_EMAIL);
-    userEvent.type(senha, VALID_PASSWORD);
+    userEvent.type(email, 'alguem@email.com');
+    userEvent.type(senha, '123456');
     expect(button).toBeEnabled();
   });
-});
 
-describe('3 - Utilize o Redux para salvar no estado global as informações da pessoa logada', () => {
   test('Salve o email no estado da aplicação, com a chave email, assim que o usuário logar.', () => {
-    const { store } = renderWithRouterAndStore(<App />, '/');
-    const email = screen.getByTestId(EMAIL_INPUT_TEST_ID);
-    const senha = screen.getByTestId(PASSWORD_INPUT_TEST_ID);
+    const { store } = renderWithRouterAndStore(<App />);
+    const email = screen.getByTestId('email-input');
+    const senha = screen.getByTestId('password-input');
     const button = screen.getByText(/Entrar/i);
 
-    userEvent.type(email, VALID_EMAIL);
-    userEvent.type(senha, VALID_PASSWORD);
+    userEvent.type(email, 'alguem@email.com');
+    userEvent.type(senha, '123456');
     fireEvent.click(button);
 
-    expect(store.getState().user.email).toBe(VALID_EMAIL);
+    expect(store.getState().user.email).toBe('alguem@email.com');
   });
 
   test('A rota deve ser mudada para \'/carteira\' após o clique no botão.', () => {
-    const { history } = renderWithRouterAndStore(<App />, '/');
-    const email = screen.getByTestId(EMAIL_INPUT_TEST_ID);
-    const senha = screen.getByTestId(PASSWORD_INPUT_TEST_ID);
+    const { history } = renderWithRouterAndStore(<App />);
+    const email = screen.getByTestId('email-input');
+    const senha = screen.getByTestId('password-input');
     const button = screen.getByText(/Entrar/i);
 
-    userEvent.type(email, VALID_EMAIL);
-    userEvent.type(senha, VALID_PASSWORD);
+    userEvent.type(email, 'alguem@email.com');
+    userEvent.type(senha, '123456');
     fireEvent.click(button);
 
     expect(history.location.pathname).toBe('/carteira');
