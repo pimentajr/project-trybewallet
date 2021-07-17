f8d08f82aa1c440ea9bee12fad0c45f3ff948711
Lais Simenikim <l.simenikim@gmail.com>
2021-07-17 19:25:56 -0300

    add requisito 6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -7,12 +7,48 @@ class Wallet extends React.Component {
     const { email } = this.props;
 
     return (
-      <header>
-        <h1>TrybeWallet</h1>
-        <p data-testid="email-field">{ email }</p>
-        <p data-testid="total-field">0</p>
-        <p data-testid="header-currency-field">BRL</p>
-      </header>
+      <div>
+        <header>
+          <h1>TrybeWallet</h1>
+          <p data-testid="email-field">{ email }</p>
+          <p data-testid="total-field">0</p>
+          <p data-testid="header-currency-field">BRL</p>
+        </header>
+        <form>
+          <label htmlFor="valor">
+            Valor
+            <input id="valor" />
+          </label>
+          <label htmlFor="descricao">
+            Descrição
+            <input id="descricao" />
+          </label>
+          <label htmlFor="moeda">
+            Moeda
+            <select id="moeda">
+              <option>teste</option>
+            </select>
+          </label>
+          <label htmlFor="pagamento">
+            Método de pagamento
+            <select id="pagamento">
+              <option>Dinheiro</option>
+              <option>Cartão de crédito</option>
+              <option>Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="tag">
+            Tag
+            <select id="tag">
+              <option>Alimentação</option>
+              <option>Lazer</option>
+              <option>Trabalho</option>
+              <option>Transporte</option>
+              <option>Saúde</option>
+            </select>
+          </label>
+        </form>
+      </div>
     );
   }
 }
