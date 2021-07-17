9089b325c7144e0d71cbbbe4f80819467bc58fec
benlopes <rubenssouss@hotmail.com>
2021-07-17 14:57:28 -0300

    minor fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Switch } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
