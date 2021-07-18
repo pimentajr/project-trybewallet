b0c075a6b4f2324e32841a34cd4761dce8ac494c
camiloLelis <lafirmafuerte@gmail.com>
2021-07-18 15:45:52 -0300

    criando

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import './App.css';
+import Login from './pages/Login';
 
-function App() {
-  return <div>Hello, TrybeWallet!</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/" render={ () => <Login /> } />
+      </Switch>
+    </div>
+  );
 }
-
-export default App;
