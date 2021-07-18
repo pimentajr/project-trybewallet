e063f9f07f45b85917d7971158c7199348ef3e0a
camiloLelis <lafirmafuerte@gmail.com>
2021-07-18 16:07:10 -0300

    requisito-1-tenta-1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,20 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <h1>Login</h1>
+        <form>
+          <input datatestid="email-input" />
+          <input datatestid="password-input" />
+          <button
+            type="button"
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
