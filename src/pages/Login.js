25b68ff3587e6bf0ad46ca37bb4c0cc4143c7b8e
igordmf <igordanielmf@gmail.com>
2021-07-18 15:30:13 -0300

    Iniciando projeto

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class Login extends React.Component {
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = ({
+      email: '',
+      password: '',
+    });
+  }
+
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <form>
+          <input type="email" data-testid="email-input" />
+          <input type="password" data-testid="password-input" />
+          <button type="button">Entrar</button>
+        </form>
+      </div>
+    );
   }
 }
 
