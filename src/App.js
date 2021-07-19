28dd51577e7a990b744951525cc5b3aa0cbe0c9e
Raphael <raphaellfpg@gmail.com>
2021-07-19 15:57:34 -0300

    Test requisito 4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 class App extends Component {
   render() {
     return (
-      <Login />
+      <Switch>
+        <Route to="/" exact component={ Login } />
+        <Route to="/carteira" component={ Wallet } />
+      </Switch>
+
     );
   }
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,14 +2,45 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      senha: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { email, senha } = this.state;
     return (
       <div>
         <form>
           Login
-          <input type="email" data-testid="email-input" />
+          <input
+            onChange={ this.handleChange }
+            value={ email }
+            type="email"
+            name="email"
+            data-testid="email-input"
+            placeholder="Digite seu email"
+          />
           Senha
-          <input type="password" data-testid="password-input" />
+          <input
+            onChange={ this.handleChange }
+            value={ senha }
+            type="password"
+            name="senha"
+            data-testid="password-input"
+            placeholder="Digite sua senha"
+          />
         </form>
         <Link to="/carteira">
           Entrar
