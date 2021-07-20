02c959f8d30421d5940048c4096245654d5e6cae
camiloLelis <lafirmafuerte@gmail.com>
2021-07-20 10:03:55 -0300

    configurar-store

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import rootReducer from '../reducers';
+import reducer from '../reducers';
 
 const store = createStore(
-  rootReducer,
+  reducer,
   composeWithDevTools(applyMiddleware(thunk)),
 );
 
