e5dd2d9f0158845c598decd82d69067e14ad8876
camiloLelis <lafirmafuerte@gmail.com>
2021-07-20 16:08:43 -0300

    requisito-2-tenta-3

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import { Provider } from 'react-redux';
-import App from './App';
+import * as App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './store';
 
