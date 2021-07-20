6b79341f02778f70a29d195a2a4f9586458c245d
fredmartins <fredweb22@gmail.com>
2021-07-20 08:47:08 -0300

    iniciando criação Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,55 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { userLogin } from '../actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.validateLogin = this.validateLogin.bind(this);
+    this.handleEmail = this.handleEmail.bind(this);
+  }
+
+  validateLogin() {
+    
+  }
+
+  handleEmail() {
+
+  }
+
   render() {
-    return <div>Login</div>;
+    const { ... } = this.state;
+    const { ... } = this.props;
+    return (
+      <div>
+        <h1>Login</h1>
+        <label >
+          E-mail
+          <input
+            ...
+          />
+        </label>
+        <label >
+          Senha
+          <input
+            ...
+          />
+        </label>
+        <Link >
+          <button >
+            Entrar
+          </button>
+        </Link>
+      </div>
+    );
   }
 }
 
-export default Login;
+
+export default (Login);
