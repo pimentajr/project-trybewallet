7f293895790d9f4c38fbaf0d08e429c1af45b6f2
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-07-21 15:29:32 -0300

    Requisito 07: Logica de filtrar as moedas

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -1,31 +1,64 @@
 import React from 'react';
 
 class WalletForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      moedas: [],
+    };
+    this.fetchMoedas = this.fetchMoedas.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchMoedas();
+  }
+
+  async fetchMoedas() {
+    const fetchApi = 'https://economia.awesomeapi.com.br/json/all';
+    const response = await fetch(fetchApi);
+    const moneyCode = await response.json();
+    delete moneyCode.USDT;
+    this.setState({ moedas: moneyCode });
+  }
+
   render() {
+    const { moedas } = this.state;
     return (
       <form>
         <label htmlFor="valor">
           Valor
-          <input type="number" name="valor" />
+          <input id="valor" type="number" name="valor" />
         </label>
         <label htmlFor="descrição">
           Descrição
-          <input type="text" name="descrição" />
+          <input id="descrição" type="text" name="descrição" />
         </label>
         <label htmlFor="moeda">
           Moeda
-          <select name="moeda">
-            <option value="vazio">Vazio</option>
+          <select id="moeda" name="moeda">
+            { Object.keys(moedas).map((moeda) => (
+              <option value={ moeda.code } key={ moeda }>{ moeda }</option>)) }
           </select>
         </label>
         <label htmlFor="metodo-de-pagamento">
           Método de pagamento
-          <select name="metodo-de-pagamento">
+          <select id="metodo-de-pagamento" name="metodo-de-pagamento">
             <option value="dinheiro">Dinheiro</option>
             <option value="cartão-debito">Cartão de débito</option>
             <option value="cartao-credito">Cartão de crédito</option>
           </select>
         </label>
+        <label htmlFor="tag">
+          Tag
+          <select id="tag" name="tag">
+            <option value="Alimentação">Alimentação</option>
+            <option value="Lazer">Lazer</option>
+            <option value="Trabalho">Trabalho</option>
+            <option value="transporte">transporte</option>
+            <option value="Saúde">Saúde</option>
+          </select>
+        </label>
+
       </form>
     );
   }
