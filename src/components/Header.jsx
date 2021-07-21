8b4ba337575e30eb50018c74576464f93fe11e75
Leonardo Lima <lima.leonardo.r@gmail.com>
2021-07-21 10:26:29 -0300

    corrige erros de dependencias

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Container } from 'react-bootstrap';
+
+class Header extends Component {
+  render() {
+    return (
+      <Container>
+        <header> tralala </header>
+      </Container>
+    );
+  }
+}
+
+export default Header;
