78bd9c51a29457d612e19acd38c84539f2d36ce7
Leonardo Lima <lima.leonardo.r@gmail.com>
2021-07-21 12:14:14 -0300

    aplica correçoes solicitadas pelo linter

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import { Container } from 'react-bootstrap';
 
 class Header extends Component {
   render() {
     return (
-      <Container>
+      <div>
         <header> tralala </header>
-      </Container>
+      </div>
     );
   }
 }
