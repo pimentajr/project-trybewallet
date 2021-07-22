5cae1626ddf04e9e3e03d9a21dbb2c195eec7c32
Luiz Putinatti <luiz_edu16@hotmail.com>
2021-07-22 01:02:24 -0300

    ajuste letra errada

--- src/components/Table.js
+++ src/components/Table.js
@@ -13,7 +13,7 @@ class Table extends React.Component {
             <th>Descrição</th>
             <th>Tag</th>
             <th>Método de pagamento</th>
-            <th>valor</th>
+            <th>Valor</th>
             <th>Moeda</th>
             <th>Câmbio utilizado</th>
             <th>Valor convertido</th>
