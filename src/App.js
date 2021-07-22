450a537f9ddbe28a243de292d2166ed46d773ad9
jorgeramires <jorgerl123@gmail.com>
2021-07-21 21:00:08 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
+import store from './store';
+// desenvolvido com ajuda do code reviem feito com o pull request victor faria
+// source https://github.com/tryber/sd-011-project-trybewallet/pull/7
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <div>
+      <Provider store={ store }>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={ Login } />
+            <Route path="/carteira" component={ Wallet } />
+          </Switch>
+        </BrowserRouter>
+      </Provider>
+    </div>
+  );
 }
 
 export default App;
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,96 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Expense from '../components/Expense';
+import ExpenseTable from '../components/ExpenseTable';
+import EditExpenses from '../components/EditExpenses';
+import fetchApi from '../services';
+import { saveCurrency } from '../actions';
 
 class Wallet extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currency: [],
+    };
+    this.handleState = this.handleState.bind(this);
+  }
+
+  async componentDidMount() {
+    const { saveObj } = this.props;
+    const apiResponse = await fetchApi();
+    const arrayOfCurrencies = Object.keys(apiResponse).map((element) => (
+      apiResponse[element]))
+      .filter(({ codein }) => codein !== 'BRLT');
+    this.handleState(arrayOfCurrencies);
+    saveObj(arrayOfCurrencies.map((currency) => currency.code));
+  }
+
+  handleState(value) {
+    this.setState(() => ({
+      currency: value,
+    }));
+  }
+
+  calculateConversion() {
+    const { expenses } = this.props;
+    const askPrices = [];
+    expenses.forEach(({ currency, exchangeRates }) => {
+      askPrices.push(exchangeRates[currency].ask);
+    });
+    return askPrices;
+  }
+
+  calculateTotal() {
+    const { expenses } = this.props;
+    const askValues = this.calculateConversion();
+    const totalConverted = expenses.reduce((acc, curr, index) => (
+      acc + Number(curr.value) * askValues[index]), 0);
+    return (Math.round(totalConverted * 100) / 100).toFixed(2);
+  }
+
   render() {
-    return <div>TrybeWallet</div>;
+    const { email, edit } = this.props;
+    const { currency } = this.state;
+    return (
+      <div>
+        <header>
+          <span data-testid="email-field">{ email }</span>
+          <br />
+          <span data-testid="total-field">
+            { this.calculateTotal() }
+          </span>
+          <span data-testid="header-currency-field">BRL</span>
+        </header>
+        <main>
+          {edit ? <EditExpenses /> : currency.length > 0 && <Expense />}
+          <ExpenseTable />
+        </main>
+      </div>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = ({ user, wallet }) => ({
+  email: user.email,
+  expenses: wallet.expenses,
+  edit: wallet.editExpense,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  saveObj: (payload) => dispatch(saveCurrency(payload)),
+});
+
+Wallet.propTypes = ({
+  email: PropTypes.string.isRequired,
+  saveObj: PropTypes.func,
+  expenses: PropTypes.arrayOf(PropTypes.object).isRequired,
+  edit: PropTypes.bool,
+});
+
+Wallet.defaultProps = ({
+  saveObj: () => {},
+  edit: false,
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Wallet);
