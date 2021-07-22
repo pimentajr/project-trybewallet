2fc9f5e6f91a95fcd9356a00b6db3e10000d85cc
Jander Liborio <jandernunes14@gmail.com>
2021-07-22 07:39:02 -0400

    req 1 teste

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,81 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: false,
+      disabled: false,
+    };
+    this.handleChangeEmail = this.handleChangeEmail.bind(this);
+    this.handleChangePassword = this.handleChangePassword.bind(this);
+  }
+
+  handleChangeEmail({ target: { value } }) {
+    this.setState({
+      email: value,
+    });
+    this.setState({ password: value.match(/[a-z]+@[a-z]+.com/g) });
+  }
+
+  handleChangePassword({ target: { value } }) {
+    const min = 6;
+
+    this.setState({ disabled: value.length >= min });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { keyEmail } = this.props;
+    const { password, disabled, email } = this.state;
+    const permit = password && disabled;
+    return (
+      <div>
+        <h2> Login TrybeWallet </h2>
+        <br />
+        <label htmlFor="email-input">
+          Email:
+          {/* {' '} */}
+          <input
+            type="email"
+            name="email"
+            data-testid="email-input"
+            onChange={ this.handleChangeEmail }
+          />
+        </label>
+        <br />
+        <label htmlFor="email-input">
+          Senha:
+          {/* {' '} */}
+          <input
+            type="password"
+            name="password"
+            data-testid="password-input"
+            onChange={ this.handleChangePassword }
+          />
+        </label>
+        <Link to="/carteira">
+          <button
+            type="button"
+            disabled={ !permit }
+            onClick={ () => keyEmail(email) }
+          >
+            Entrar
+          </button>
+        </Link>
+      </div>);
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  gEmail: (email) => dispatch(getEmail(email)),
+});
+
+export default connect(null, mapDispatchToProps)(Login);
+
+Login.propTypes = {
+  gEmail: PropTypes.func,
+}.isRequired;
