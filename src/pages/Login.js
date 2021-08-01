674f284639c6dae4177860610af305578de15456
JuliaBaptista <juliabaptistadeoliveira@gmail.com>
2021-08-01 19:39:51 -0300

    Req 01

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,34 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <h1>Trybe Wallet</h1>
+        <form>
+          <label htmlFor="email">
+            Login:
+            <input
+              type="email"
+              data-testid="email-input"
+              name="email"
+            />
+          </label>
+          <label htmlFor="password">
+            Senha:
+            <input
+              type="password"
+              data-testid="password-input"
+              name="password"
+            />
+          </label>
+          <button
+            type="submit"
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
