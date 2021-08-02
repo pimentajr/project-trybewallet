b7b9e7ecbd233ef7dc7b56cd0aca1aa25b47e6c9
JuliaBaptista <juliabaptistadeoliveira@gmail.com>
2021-08-02 14:46:53 -0300

    Req-09

---
+++ src/components/Table.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+class Table extends Component {
+  constructor() {
+    super();
+
+    this.renderTBody = this.renderTBody.bind(this);
+    this.getExchangeRateData = this.getExchangeRateData.bind(this);
+  }
+
+  getExchangeRateData({ currency, exchangeRates }, property) {
+    return exchangeRates[currency][property];
+  }
+
+  renderTBody() {
+    const { expenses } = this.props;
+
+    return (
+      <tbody>
+        {expenses.length === 0 ? <span>Login</span>
+          : expenses.map((expense) => {
+            const { id, value, description, method, tag } = expense;
+            return (
+              <tr key={ id }>
+                <td>{ description }</td>
+                <td>{ tag }</td>
+                <td>{ method }</td>
+                <td>
+                  { Math.round(value * 100) / 100 }
+                </td>
+                <td>
+                  { this.getExchangeRateData(expense, 'name') }
+                </td>
+                <td>
+                  { (Math.round(
+                    this.getExchangeRateData(expense, 'ask') * 100,
+                  ) / 100).toFixed(2) }
+                </td>
+                <td>
+                  { (Math.round(
+                    value * this.getExchangeRateData(expense, 'ask') * 100,
+                  ) / 100).toFixed(2) }
+                </td>
+                <td>Real</td>
+              </tr>
+            );
+          })}
+      </tbody>
+    );
+  }
+
+  render() {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        {this.renderTBody()}
+      </table>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default connect(mapStateToProps)(Table);
