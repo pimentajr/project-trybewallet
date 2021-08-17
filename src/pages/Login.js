d9cb6a65f99a100d1bf9ef2b26629c73068f52cd
Vitor HUgo <vitor.h.braz.leite@gmail.com>
2021-08-17 09:11:56 -0300

    Add Req 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,63 @@
 import React from 'react';
+import { Redirect } from 'react-router';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import * as userAction from '../actions/index';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      isDisabled: true,
+      shouldRedirectToWallet: false,
+    };
+    this.shouldEnableButton = this.shouldEnableButton.bind(this);
+    this.redirectToWallet = this.redirectToWallet.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    this.setState({ [target.name]: target.value }, () => this.shouldEnableButton());
+  }
+
+  verifyEmail(email) {
+    const emailRegex = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
+    return emailRegex.test(email);
+  }
+
+  shouldEnableButton() {
+    const { email, password } = this.state;
+    const minValue = 6;
+    if (this.verifyEmail(email) && password.length >= minValue) {
+      return this.setState({ isDisabled: false });
+    }
+    return this.setState({ isDisabled: true });
+  }
+
+  redirectToWallet() {
+    const { saveEmail } = this.props;
+    const { email } = this.state;
+    saveEmail(email);
+    this.setState({
+      shouldRedirectToWallet: true,
+    });
+  }
+
   render() {
+    const { isDisabled, shouldRedirectToWallet } = this.state;
     return (
       <div>
         <label htmlFor="email">
           Email:
-          <input type="email" id="email" data-testid="email-input" />
+          <input
+            name="email"
+            type="email"
+            id="email"
+            data-testid="email-input"
+            onChange={ this.handleChange }
+          />
         </label>
         {' '}
         <br />
@@ -14,16 +65,37 @@ class Login extends React.Component {
         <br />
         <label htmlFor="password">
           Senha:
-          <input type="password" id="password" data-testid="password-input" />
+          <input
+            name="password"
+            type="password"
+            id="password"
+            data-testid="password-input"
+            onChange={ this.handleChange }
+          />
         </label>
         {' '}
         <br />
         {' '}
         <br />
-        <button type="button">Entrar</button>
+        <button
+          type="button"
+          disabled={ isDisabled }
+          onClick={ () => this.redirectToWallet() }
+        >
+          Entrar
+        </button>
+        <span>{ shouldRedirectToWallet ? <Redirect to="/carteira" /> : <div /> }</span>
       </div>
     );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({ // mapeia as minha ações que serão disparadas
+  saveEmail: (email) => dispatch(userAction.login(email)),
+});
+
+Login.propTypes = {
+  saveEmail: PropTypes.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
--- src/tests/login.test.js
+++ src/tests/login.test.js
@@ -35,62 +35,62 @@ describe('1 - Crie uma página inicial de login com os seguintes campos e caract
   });
 });
 
-// describe('2 - Realize as seguintes verificações nos campos de email, senha e botão:', () => {
-//   test('O botão de "Entrar" está desabilitado ao entrar na página', () => {
-//     renderWithRouterAndStore(<App />, '/');
+describe('2 - Realize as seguintes verificações nos campos de email, senha e botão:', () => {
+  test('O botão de "Entrar" está desabilitado ao entrar na página', () => {
+    renderWithRouterAndStore(<App />, '/');
 
-//     const button = screen.getByText(/Entrar/i);
-//     expect(button).toBeDisabled();
-//   });
+    const button = screen.getByText(/Entrar/i);
+    expect(button).toBeDisabled();
+  });
 
-//   test('O botão de "Entrar está desabilitado quando um email inválido é digitado', () => {
-//     renderWithRouterAndStore(<App />, '/');
+  test('O botão de "Entrar está desabilitado quando um email inválido é digitado', () => {
+    renderWithRouterAndStore(<App />, '/');
 
-//     const email = screen.getByTestId(EMAIL_INPUT_TEST_ID);
-//     const senha = screen.getByTestId(PASSWORD_INPUT_TEST_ID);
-//     const button = screen.getByText(/Entrar/i);
+    const email = screen.getByTestId(EMAIL_INPUT_TEST_ID);
+    const senha = screen.getByTestId(PASSWORD_INPUT_TEST_ID);
+    const button = screen.getByText(/Entrar/i);
 
-//     userEvent.type(email, 'email');
-//     userEvent.type(senha, VALID_PASSWORD);
-//     expect(button).toBeDisabled();
+    userEvent.type(email, 'email');
+    userEvent.type(senha, VALID_PASSWORD);
+    expect(button).toBeDisabled();
 
-//     userEvent.type(email, 'email@com@');
-//     userEvent.type(senha, VALID_PASSWORD);
-//     expect(button).toBeDisabled();
+    userEvent.type(email, 'email@com@');
+    userEvent.type(senha, VALID_PASSWORD);
+    expect(button).toBeDisabled();
 
-//     userEvent.type(email, 'emailcom@');
-//     userEvent.type(senha, VALID_PASSWORD);
-//     expect(button).toBeDisabled();
+    userEvent.type(email, 'emailcom@');
+    userEvent.type(senha, VALID_PASSWORD);
+    expect(button).toBeDisabled();
 
-//     userEvent.type(email, 'alguem@email.');
-//     userEvent.type(senha, VALID_PASSWORD);
-//     expect(button).toBeDisabled();
-//   });
+    userEvent.type(email, 'alguem@email.');
+    userEvent.type(senha, VALID_PASSWORD);
+    expect(button).toBeDisabled();
+  });
 
-//   test('O botão de "Entrar está desabilitado quando uma senha inválida é digitada', () => {
-//     renderWithRouterAndStore(<App />, '/');
+  test('O botão de "Entrar está desabilitado quando uma senha inválida é digitada', () => {
+    renderWithRouterAndStore(<App />, '/');
 
-//     const email = screen.getByTestId(EMAIL_INPUT_TEST_ID);
-//     const senha = screen.getByTestId(PASSWORD_INPUT_TEST_ID);
-//     const button = screen.getByText(/Entrar/i);
+    const email = screen.getByTestId(EMAIL_INPUT_TEST_ID);
+    const senha = screen.getByTestId(PASSWORD_INPUT_TEST_ID);
+    const button = screen.getByText(/Entrar/i);
 
-//     userEvent.type(email, VALID_EMAIL);
-//     userEvent.type(senha, '23456');
-//     expect(button).toBeDisabled();
-//   });
+    userEvent.type(email, VALID_EMAIL);
+    userEvent.type(senha, '23456');
+    expect(button).toBeDisabled();
+  });
 
-//   test('O botão de "Entrar" está habilitado quando um email e uma senha válidos são passados', () => {
-//     renderWithRouterAndStore(<App />, '/');
+  test('O botão de "Entrar" está habilitado quando um email e uma senha válidos são passados', () => {
+    renderWithRouterAndStore(<App />, '/');
 
-//     const email = screen.getByTestId(EMAIL_INPUT_TEST_ID);
-//     const senha = screen.getByTestId(PASSWORD_INPUT_TEST_ID);
-//     const button = screen.getByText(/Entrar/i);
+    const email = screen.getByTestId(EMAIL_INPUT_TEST_ID);
+    const senha = screen.getByTestId(PASSWORD_INPUT_TEST_ID);
+    const button = screen.getByText(/Entrar/i);
 
-//     userEvent.type(email, VALID_EMAIL);
-//     userEvent.type(senha, VALID_PASSWORD);
-//     expect(button).toBeEnabled();
-//   });
-// });
+    userEvent.type(email, VALID_EMAIL);
+    userEvent.type(senha, VALID_PASSWORD);
+    expect(button).toBeEnabled();
+  });
+});
 
 // describe('3 - Utilize o Redux para salvar no estado global as informações da pessoa logada', () => {
 //   test('Salve o email no estado da aplicação, com a chave email, assim que o usuário logar.', () => {
